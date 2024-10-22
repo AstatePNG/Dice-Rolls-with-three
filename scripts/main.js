7f7e6ecec52c3ea33333ca822d5d8d3cf7c4fceb
Elsa Hamon <elsa.hamon@etu.unicaen.fr>
2024-10-22 19:16:28 +0200

    remove of the background color, made the renderer overlap the webpage content and add of a light

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,32 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene({alpha: true});
-scene.background = ({color : 0x00000});
-scene.backgroundIntensity = 0;
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 300 );
+camera.position.set(0,.5,4);
+
+const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+
 document.body.appendChild( renderer.domElement );
 
+const light = new THREE.PointLight(0xffffff, 5, 50);
+light.position.set(0,.5,4);
+light.castShadow = true;
+scene.add(light);
+
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 500;
+
+
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
+cube.receiveShadow = true;
 scene.add( cube );
 
 camera.position.z = 5;
